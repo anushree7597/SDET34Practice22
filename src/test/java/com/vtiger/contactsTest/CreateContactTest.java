61d06ec309f35e902dd19415f2887e4e93c61316
DELL <DELL@Dell3568i37thgen>
2022-05-31 16:31:39 +0530

    changes are made in sdet3422

---
+++ src/test/java/com/vtiger/contactsTest/CreateContactTest.java
@@ -0,0 +1,48 @@
+package com.vtiger.contactsTest;
+
+import java.io.IOException;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+import com.sdet34l1.genericUtility.BaseClass;
+import com.sdet34l1.genericUtility.ExcelsheetUtility;
+import com.sdet34l1.genericUtility.FileDataUtility;
+import com.sdet34l1.genericUtility.IconstantPathOfPropertyUtility;
+import com.sdet34l1.genericUtility.JavaJdkUtility;
+import com.sdet34l1.genericUtility.WebDriverBrowserUtility;
+import com.vtiger.ObjectRepository.ClickOnCreateProductPage;
+import com.vtiger.ObjectRepository.ClickOnNewContactPage;
+import com.vtiger.ObjectRepository.ContactLastNamePage;
+import com.vtiger.ObjectRepository.HomePage;
+import com.vtiger.ObjectRepository.LoginPage;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
+
+public class CreateContactTest extends BaseClass
+{
+	String lastname;
+	ClickOnNewContactPage contactbutton;
+	ContactLastNamePage enterlastcontactname;
+	ContactLastNamePage save;
+
+	@Test
+	public  void createcontactTest()
+	{
+		String lastname = ExcelsheetUtility.getDataFromExcel("Organization", 4, 2)+randomNumber;
+		contactbutton=new ClickOnNewContactPage(driver);
+		enterlastcontactname=new ContactLastNamePage(driver);
+		save=new ContactLastNamePage(driver);
+		homePage.clickContactTab();
+		contactbutton.clickNewcontactButton();
+		enterlastcontactname.enterLastName(lastname);
+		save.saveLastName();
+	}
+}
